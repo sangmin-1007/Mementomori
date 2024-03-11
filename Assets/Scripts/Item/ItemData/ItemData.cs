f6f469c33103e754216a727845080ff4f19f93e4
LoopySub <153615396+LoopySub@users.noreply.github.com>
2024-03-11 20:51:31 +0900

    [feat]아이템데이터 추가

--- Assets/Scripts/Item/ItemData/ItemData.cs
+++ Assets/Scripts/Item/ItemData/ItemData.cs
@@ -7,22 +7,49 @@ using UnityEngine;
 public class ItemData
 {
 
-    [SerializeField] private int _itemType;
+  
     [SerializeField] private int _id;
     [SerializeField] private string _grade;
+    [SerializeField] private string _type;
     [SerializeField] private string _name;
     [SerializeField] private string _description;
-    [SerializeField] private string _stringPath;
-    [SerializeField] private float _value;
+    [SerializeField] private string _spritePath;
+    [SerializeField] private float _value1;
+    [SerializeField] private float _value2;
+    [SerializeField] private float _value3;
+    [SerializeField] private float _value4;
+    [SerializeField] private float _value5;
+    [SerializeField] private float _value6;
 
-    public int Type => _itemType;
+
     public int Id => _id;
     public string Grade => _grade;
+    public string Type => _type;
     public string Name => _name;
     public string Description => _description;
-    public float Value => _value;
-        
-    
-        
+
+    public string SpritePath => _spritePath;
+    public float Value1=> _value1;
+
+    public float Value2 => _value2;
+    public float Value3 => _value3;
+    public float Value4 => _value4;
+    public float Value5 => _value5;
+    public float Value6 => _value6;
+
+    private Sprite _sprite;
+
+    public Sprite Sprite
+    {
+        get
+        {
+            if (_sprite == null)
+               _sprite = Resources.Load<Sprite>(SpritePath);
+            return _sprite;
+        }
+    }
+
+
+
 
 }
--- Assets/Scripts/Item/RawData/ItemDBSheet.cs
+++ Assets/Scripts/Item/RawData/ItemDBSheet.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExcelAsset]
+[ExcelAsset(AssetPath = "Resources/DB", ExcelName ="ItemDBSheet")]
 public class ItemDBSheet : ScriptableObject
 {
 	public List<ItemData> Entities; // Replace 'EntityType' to an actual type that is serializable.
