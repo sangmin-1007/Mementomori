17e1fb8ae03137cb7a1a62a5270b406342dc35ba
sangmin-1007 <dosin0822@gmail.com>
2024-03-14 17:24:26 +0900

    feat : UI_HUD 추가
    Player의 좌표는 임시로 Inspecter에 Player를 등록해서 사용

---
+++ Assets/Scripts/UI/UI_HUD.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_HUD : UI_Base<UI_HUD>
+{
+    [SerializeField] private CanvasGroup miniMapCanvasGroup;
+    [SerializeField] private Button miniMapSubButton;
+    [SerializeField] private Button miniMapAddButton;
+
+    [SerializeField] private Image hpBar;
+    [SerializeField] private Image staminaBar;
+    [SerializeField] private Image expBar;
+
+    private int curHp, maxHP;
+    private int curStamina, maxStamina;
+    private int curExp, maxExp;
+
+    private void Start()
+    {
+        miniMapAddButton.onClick.AddListener(OnClickAddButton);
+        miniMapSubButton.onClick.AddListener(OnClickSubButton);
+    }
+
+    private void Update()
+    {
+        hpBar.fillAmount = Mathf.Lerp(hpBar.fillAmount, 0, Time.deltaTime * 3f);
+        staminaBar.fillAmount = Mathf.Lerp(staminaBar.fillAmount, 0, Time.deltaTime * 5f);
+        expBar.fillAmount = Mathf.Lerp(expBar.fillAmount, 1f, Time.deltaTime * 1.5f);
+
+        if(expBar.fillAmount >= 0.99f)
+        {
+            expBar.fillAmount = 0f;
+        }
+    }
+
+    private float GetPercentage(float curValue, float maxValue)
+    {
+        return curValue/ maxValue;
+    }
+
+    private void OnClickAddButton()
+    {
+        if(miniMapCanvasGroup.alpha <= 1f)
+        {
+            miniMapCanvasGroup.alpha += 0.1f;
+        }
+    }
+
+    private void OnClickSubButton()
+    {
+        if(miniMapCanvasGroup.alpha >= 0 )
+        {
+            miniMapCanvasGroup.alpha -= 0.1f;
+        }
+        
+    }
+}
