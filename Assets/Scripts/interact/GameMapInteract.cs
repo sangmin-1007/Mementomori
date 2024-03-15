696981d246f341ac661a472cd63732ae5de770fc
sangmin-1007 <dosin0822@gmail.com>
2024-03-15 20:13:21 +0900

    feat : 맵이동 트리거 추가.

---
+++ Assets/Scripts/interact/GameMapInteract.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameMapInteract : MonoBehaviour
+{
+    public InteractType interactType;
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private PlayerController PlayerInput;
+
+    private GameMapInteract gameMapInteract;
+    private Vector2 PlayerDir;
+    private void Start()
+    {
+        PlayerInput.OnMoveEvent += PlayerMoveDir;
+    }
+
+    public void Interaction()
+    {
+        Vector3 myPos = transform.position;
+        Vector3 playerPos = player.transform.position;
+
+        float diffX = Mathf.Abs(playerPos.x - myPos.x);
+        float diffY = Mathf.Abs(playerPos.y - myPos.y);
+
+        float dirX = PlayerDir.x < 0 ? -1 : 1;
+        float dirY = PlayerDir.y < 0 ? -1 : 1;
+
+        switch(interactType)
+        {
+            case InteractType.GameMapReposition:
+                if (diffX > diffY)
+                {
+                    transform.Translate(Vector3.right * dirX * 40f);
+                }
+                else if (diffX < diffY)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void PlayerMoveDir(Vector2 dir)
+    {
+        PlayerDir = dir;
+    }
+
+}
