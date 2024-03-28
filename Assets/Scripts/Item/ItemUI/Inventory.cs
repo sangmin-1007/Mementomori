5491d5adfb976ad43234066b6073d3fd110ad4a4
LoopySub <153615396+LoopySub@users.noreply.github.com>
2024-03-28 23:36:21 +0900

    [feat] 아이템 마우스올려놓을 시 상세정보 노출

--- Assets/Scripts/Item/ItemUI/Inventory.cs
+++ Assets/Scripts/Item/ItemUI/Inventory.cs
@@ -1,4 +1,5 @@
 using Constants;
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -18,15 +19,20 @@ public class ItemSlot
 public class Inventory : UI_Base<Inventory>
 {
     public ItemSlotUI[] uiSlots;
-    public ItemSlotUI[] equippedSlots;
     public ItemSlot[] slots;
-   
-   
+
+    public ItemSlotUI[] equippedSlots;
+    public ItemSlot[] equipSlots;
+    
+
+
     public GameObject inventoryWindow;
 
     [Header("Selected Item")]
     private ItemSlot selectedItem;
+    private ItemSlot equipSelectedItem;
     private int selectedItemIndex;
+    private int equipSelectedItemIndex;
     
     public Text selectedItemName;
     public Text selectedItemDescription;
@@ -34,12 +40,17 @@ public class Inventory : UI_Base<Inventory>
     public Text[] selectedItemStatName;
     public Text[] selectedItemStatValue;
 
+    public GameObject ToolTip;
+
     public GameObject equipButton;
     public GameObject unequipButton;
  
     private SpriteRenderer spriteRenderer;
     
     private int curEquipIndex;
+    private Outline _outline;
+
+    
 
     //private PlayerController controller; 캐릭터 InvokeEvent방식이랑 호환
     //private PlayerCondition condition; 장비창이 스탯이랑 연관있으니 캐릭터 스탯?이랑 호환해야할듯
@@ -53,9 +64,10 @@ public class Inventory : UI_Base<Inventory>
     void Awake()
     {
         instance = this;
-
+        _outline = ToolTip.GetComponentInChildren<Outline>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         slots = new ItemSlot[uiSlots.Length];
+        equipSlots = new ItemSlot[equippedSlots.Length];
 
         for (int i = 0; i < uiSlots.Length; i++)
         {
@@ -63,6 +75,13 @@ public class Inventory : UI_Base<Inventory>
             uiSlots[i].index = i;
             uiSlots[i].Clear();
         }
+        for (int i = 0; i < equippedSlots.Length; i++)
+        {
+            equipSlots[i] = new ItemSlot();
+            equippedSlots[i].index = i;
+            equippedSlots[i].Clear();
+        }
+
         ClearSeletecItemWindow();
     }
 
@@ -132,6 +151,20 @@ public class Inventory : UI_Base<Inventory>
             else
                 uiSlots[i].Clear();
         }
+
+        for (int i = 0; i < equipSlots.Length; i++)
+        {
+
+            if (equipSlots[i].item != null) 
+            {
+                equippedSlots[i].Set(equipSlots[i]);
+
+            }
+            else
+                equippedSlots[i].Clear();
+        }
+
+
     }
 
    
@@ -147,29 +180,145 @@ public class Inventory : UI_Base<Inventory>
         return null;
     }
 
-    public void SelectItem(int index)
+ 
+
+ 
+
+
+    public void SelectItem(int index,Vector3 _pos)
     {
+      
+            ToolTip.SetActive(true);
+            _pos += new Vector3(ToolTip.GetComponent<RectTransform>().rect.width * 0.5f,
+                -ToolTip.GetComponent<RectTransform>().rect.height * 0.5f);
+            ToolTip.transform.position = _pos;
+
+
         if (slots[index].item == null)
         {
-            selectedItemName.gameObject.SetActive(false);
-            selectedItemDescription.gameObject.SetActive(false);
-            for(int i = 0; i < selectedItemStatName.Length; i++)
+
+            ToolTip.SetActive(false);
+        }
+      
+
+    
+
+            selectedItem = slots[index];
+            selectedItemIndex = index;
+
+            Color Color = Colors.ItemGrade[(int)selectedItem.item.Grade];
+             selectedItemName.color = Color;
+             _outline.effectColor = Color;
+
+
+            selectedItemName.text = selectedItem.item.Name;
+            selectedItemDescription.text = selectedItem.item.Description;
+            
+
+
+
+            for (int i = 0; i < selectedItemStatName.Length; i++)
             {
-                selectedItemStatName[i].gameObject.SetActive(false);
-                selectedItemStatValue[i].gameObject.SetActive(false);
+                selectedItemStatName[i].gameObject.SetActive(true);
+                selectedItemStatValue[i].gameObject.SetActive(true);
             }
-            return;
+
+
+            if (selectedItem.item.Type == ItemType.Weapon) // 아이템이 검일때 검은 공격력/공격속도만 보여준다.
+            {
+
+                selectedItemStatName[0].text = "공격력 : ";
+                selectedItemStatName[1].text = "공격속도 :";
+
+                selectedItemStatValue[0].text = selectedItem.item.Atk.ToString();
+                selectedItemStatValue[1].text = selectedItem.item.AtkSpeed.ToString();
+
+
+            }
+
+            if (selectedItem.item.Type == ItemType.Armor) // 갑옷 = 체력
+            {
+                selectedItemName.gameObject.SetActive(true);
+                selectedItemDescription.gameObject.SetActive(true);
+
+                selectedItemStatName[0].text = "체력 : ";
+                selectedItemStatName[1].gameObject.SetActive(false);
+
+                selectedItemStatValue[0].text = selectedItem.item.Hp.ToString();
+                selectedItemStatValue[1].gameObject.SetActive(false);
+            }
+
+            if (selectedItem.item.Type == ItemType.Shield) // 방패 = 방어력
+            {
+                selectedItemName.gameObject.SetActive(true);
+                selectedItemDescription.gameObject.SetActive(true);
+
+                selectedItemStatName[0].text = "방어력 : ";
+                selectedItemStatName[1].gameObject.SetActive(false);
+
+                selectedItemStatValue[0].text = selectedItem.item.Def.ToString();
+                selectedItemStatValue[1].gameObject.SetActive(false);
+            }
+
+            if (selectedItem.item.Type == ItemType.Boots) // 신발 = 스태미나 + 이동속도
+            {
+                selectedItemName.gameObject.SetActive(true);
+                selectedItemDescription.gameObject.SetActive(true);
+
+                selectedItemStatName[0].text = "이동속도 : ";
+                selectedItemStatName[1].text = "스태미나 : ";
+
+                selectedItemStatValue[0].text = selectedItem.item.Speed.ToString();
+                selectedItemStatValue[1].text = selectedItem.item.Stamina.ToString();
+            }
         }
 
-       
-        selectedItem = slots[index];
+    public void SelectEquipItem(int index, Vector3 _pos)
+    {
+        ToolTip.SetActive(true);
+        _pos += new Vector3(ToolTip.GetComponent<RectTransform>().rect.width * 0.5f,
+            -ToolTip.GetComponent<RectTransform>().rect.height * 0.5f);
+        ToolTip.transform.position = _pos;
+
+
+        if (equipSlots[index].item == null)
+        {
+            ToolTip.SetActive(false);
+            //selectedItemName.gameObject.SetActive(false);
+            //selectedItemDescription.gameObject.SetActive(false);
+
+
+            //for (int i = 0; i < selectedItemStatName.Length; i++)
+            //{
+            //    selectedItemStatName[i].gameObject.SetActive(false);
+            //    selectedItemStatValue[i].gameObject.SetActive(false);
+            //}
+            //return;
+
+        }
+
+
+
+        selectedItem = equipSlots[index];
         selectedItemIndex = index;
 
-        selectedItemName.text = selectedItem.item.Name;
-        selectedItemDescription.text = selectedItem.item.Description;
 
-        selectedItemName.gameObject.SetActive(true);
-        selectedItemDescription.gameObject.SetActive(true);
+
+        equipSelectedItem = equipSlots[index];
+        equipSelectedItemIndex = index;
+
+        Color GradeColorEquipSelected = Colors.ItemGrade[(int)equipSelectedItem.item.Grade];
+        selectedItemName.color = GradeColorEquipSelected;
+        _outline.effectColor = GradeColorEquipSelected;
+
+        selectedItemName.text = equipSelectedItem.item.Name;
+
+        selectedItemDescription.text = equipSelectedItem.item.Description;
+       
+
+
+
+
 
         for (int i = 0; i < selectedItemStatName.Length; i++)
         {
@@ -178,16 +327,19 @@ public class Inventory : UI_Base<Inventory>
         }
 
 
-        if (selectedItem.item.Type == ItemType.Weapon) // 아이템이 검일때 검은 공격력/공격속도만 보여준다.
+        if (equipSelectedItem.item.Type == ItemType.Weapon) // 아이템이 검일때 검은 공격력/공격속도만 보여준다.
         {
+
             selectedItemStatName[0].text = "공격력 : ";
             selectedItemStatName[1].text = "공격속도 :";
 
-            selectedItemStatValue[0].text = selectedItem.item.Atk.ToString();
-            selectedItemStatValue[1].text = selectedItem.item.AtkSpeed.ToString();
+            selectedItemStatValue[0].text = equipSelectedItem.item.Atk.ToString();
+            selectedItemStatValue[1].text = equipSelectedItem.item.AtkSpeed.ToString();
+
+
         }
 
-        if(selectedItem.item.Type == ItemType.Armor) // 갑옷 = 체력
+        if (equipSelectedItem.item.Type == ItemType.Armor) // 갑옷 = 체력
         {
             selectedItemName.gameObject.SetActive(true);
             selectedItemDescription.gameObject.SetActive(true);
@@ -195,11 +347,11 @@ public class Inventory : UI_Base<Inventory>
             selectedItemStatName[0].text = "체력 : ";
             selectedItemStatName[1].gameObject.SetActive(false);
 
-            selectedItemStatValue[0].text = selectedItem.item.Hp.ToString();
+            selectedItemStatValue[0].text = equipSelectedItem.item.Hp.ToString();
             selectedItemStatValue[1].gameObject.SetActive(false);
         }
 
-        if(selectedItem.item.Type == ItemType.Shield) // 방패 = 방어력
+        if (equipSelectedItem.item.Type == ItemType.Shield) // 방패 = 방어력
         {
             selectedItemName.gameObject.SetActive(true);
             selectedItemDescription.gameObject.SetActive(true);
@@ -207,11 +359,11 @@ public class Inventory : UI_Base<Inventory>
             selectedItemStatName[0].text = "방어력 : ";
             selectedItemStatName[1].gameObject.SetActive(false);
 
-            selectedItemStatValue[0].text = selectedItem.item.Def.ToString();
+            selectedItemStatValue[0].text = equipSelectedItem.item.Def.ToString();
             selectedItemStatValue[1].gameObject.SetActive(false);
         }
 
-        if(selectedItem.item.Type == ItemType.Boots) // 신발 = 스태미나 + 이동속도
+        if (equipSelectedItem.item.Type == ItemType.Boots) // 신발 = 스태미나 + 이동속도
         {
             selectedItemName.gameObject.SetActive(true);
             selectedItemDescription.gameObject.SetActive(true);
@@ -219,23 +371,32 @@ public class Inventory : UI_Base<Inventory>
             selectedItemStatName[0].text = "이동속도 : ";
             selectedItemStatName[1].text = "스태미나 : ";
 
-            selectedItemStatValue[0].text = selectedItem.item.Speed.ToString();
-            selectedItemStatValue[1].text = selectedItem.item.Stamina.ToString();
+            selectedItemStatValue[0].text = equipSelectedItem.item.Speed.ToString();
+            selectedItemStatValue[1].text = equipSelectedItem.item.Stamina.ToString();
         }
-       
-
-        equipButton.SetActive(selectedItem.item.Type != ItemType.Consume && !uiSlots[index].equipped);
     }
 
-    private void ClearSeletecItemWindow()
+
+
+
+
+
+
+
+
+
+
+
+    public void ClearSeletecItemWindow()
     {
+        ToolTip.SetActive(false);
         selectedItem = null;
         selectedItemName.text = string.Empty;
         selectedItemDescription.text = string.Empty;
 
 
-        equipButton.SetActive(false);
-        unequipButton.SetActive(false);
+       // equipButton.SetActive(false);
+       // unequipButton.SetActive(false);
       
     }
 
@@ -255,14 +416,14 @@ public class Inventory : UI_Base<Inventory>
         Managers.DataManager.EquipItem(selectedItem.item);
         uiSlots[selectedItemIndex].icon.gameObject.SetActive(false);
         slots[selectedItemIndex].item = null;
-
+        
         UpdateEquipSlots();
         UpdateItemUI();
 
 
         //UpdateUI();
 
-        SelectItem(selectedItemIndex);
+        //SelectItem(selectedItemIndex);
     }
 
     void UnEquip(int index)
@@ -271,8 +432,8 @@ public class Inventory : UI_Base<Inventory>
         
         UpdateUI();
 
-        if (selectedItemIndex == index)
-            SelectItem(index);
+        //if (selectedItemIndex == index)
+            //SelectItem(index);
     }
 
     public void OnUnequipButton()
@@ -285,7 +446,7 @@ public class Inventory : UI_Base<Inventory>
         return false;
     }
 
-    private void UpdateItemUI()
+    public void UpdateItemUI()
     {
         for (int i = 0; i < uiSlots.Length; i++)
         {
@@ -297,36 +458,59 @@ public class Inventory : UI_Base<Inventory>
             }
         }
 
-        for (int i = 0; i <  slots.Length; i++)
+        for (int i = 0; i < slots.Length; i++)
         {
             if (i < Managers.DataManager.playerInventoryItemData.Count &&
                Managers.DataManager.playerInventoryItemData[i].Sprite != uiSlots[i].icon.sprite)
 
                 AddItem(Managers.DataManager.playerInventoryItemData[i]);
+         
         }
+
+
+
+      
+
     }
+
+ 
 
     private void UpdateEquipSlots()
     {
-        for(int i = 0; i < equippedSlots.Length; i++)
+
+
+        for (int i = 0; i < equippedSlots.Length; i++)
         {
+            
+
             if (Managers.DataManager.playerEquipItemDatas.ContainsKey(ItemType.Weapon))
             {
                 equippedSlots[0].icon.sprite = Managers.DataManager.playerEquipItemDatas[ItemType.Weapon].Sprite;
+                equipSlots[0].item = Managers.DataManager.playerEquipItemDatas[ItemType.Weapon];
+
+
             }
             if (Managers.DataManager.playerEquipItemDatas.ContainsKey(ItemType.Armor))
             {
                 equippedSlots[1].icon.sprite = Managers.DataManager.playerEquipItemDatas[ItemType.Armor].Sprite;
+                equipSlots[1].item = Managers.DataManager.playerEquipItemDatas[ItemType.Armor];
+
             }
             if (Managers.DataManager.playerEquipItemDatas.ContainsKey(ItemType.Shield))
             {
                 equippedSlots[2].icon.sprite = Managers.DataManager.playerEquipItemDatas[ItemType.Shield].Sprite;
+                equipSlots[2].item = Managers.DataManager.playerEquipItemDatas[ItemType.Shield];
             }
             if (Managers.DataManager.playerEquipItemDatas.ContainsKey(ItemType.Boots))
             {
                 equippedSlots[3].icon.sprite = Managers.DataManager.playerEquipItemDatas[ItemType.Boots].Sprite;
+                equipSlots[3].item = Managers.DataManager.playerEquipItemDatas[ItemType.Boots];
+
             }
+            
         }
+
+       
 
     }
 
