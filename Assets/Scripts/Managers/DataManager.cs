b76f743b86e6d87a3ed4f4af3bd0b92fbad658e9
sangmin-1007 <dosin0822@gmail.com>
2024-04-02 15:35:58 +0900

    Test : DataManager.cs 저장하는 TestCode 추가

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SaveData
 {
     public List<ItemData> inventoryItemData = new List<ItemData>();
-    public List<ItemData> EquipItemData = new List<ItemData>();
+    public List<ItemData> equipItemData = new List<ItemData>();
     public List<ItemData> storageItemData = new List<ItemData>();
 
     public int playerGold;
@@ -16,9 +16,33 @@ public class DataManager : MonoBehaviour
 {
     string path;
 
+    private void Start()
+    {
+
+    }
+
     public void Save()
     {
         SaveData saveData = new SaveData();
+
+        saveData.playerGold = Managers.UserData.playerGold;
+
+        for (int i = 0; i < Managers.UserData.playerInventoryItemData.Count; i++)
+        {
+            saveData.inventoryItemData.Add(Managers.UserData.playerInventoryItemData[i]);
+        }
+
+        foreach(ItemData item in Managers.UserData.playerEquipItemDatas.Values)
+        {
+            saveData.equipItemData.Add(item);
+        }
+
+        for(int i = 0; i < Managers.UserData.storageItemData.Count; i++)
+        {
+            saveData.storageItemData.Add(Managers.UserData.storageItemData[i]);
+        }
+
+
     }
 
     public void Load()
