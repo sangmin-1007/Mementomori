e62223a14898c92bdca06c761fc07159f0f038e6
sangmin-1007 <dosin0822@gmail.com>
2024-04-08 18:28:29 +0900

    feat : UI_Stats의 Text와 Player Stat연동

--- Assets/Scripts/UI/UI_Stats.cs
+++ Assets/Scripts/UI/UI_Stats.cs
@@ -2,26 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Build;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UI_Stats : UI_Base<UI_Stats>
 {
     [Header("бс Stats")]
-    [SerializeField] private Text attakStatText;
+    [SerializeField] private Text attackStatText;
     [SerializeField] private Text attackSpeedStatText;
     [SerializeField] private Text maxHpStatText;
     [SerializeField] private Text maxStanminaStatText;
     [SerializeField] private Text defStatText;
     [SerializeField] private Text moveSpeedText;
 
-    public void Start()
+    [SerializeField] private Text deathCount;
+
+    private PlayerStatsHandler playerStats;
+
+    public override void OnEnable()
     {
-        
+        base.OnEnable();
+
+       if(SceneManager.GetActiveScene().name == "LobbyScene")
+       {
+            playerStats = Managers.LobbySceneManager.Player.GetComponent<PlayerStatsHandler>();
+       }
+       else if(SceneManager.GetActiveScene().name == "GaemScene")
+       {
+            playerStats = Managers.GameSceneManager.Player.GetComponent<PlayerStatsHandler>();
+       }
+
+        UpdateStatText();
     }
 
-    public void Update()
+    private void UpdateStatText()
     {
-        
+        deathCount.text = Managers.UserData.playerDeathCount.ToString();
+        PlayerEquipStatsManager equipStat = Managers.PlayerEquipStatsManager;
+
+
+        attackStatText.text = $"{playerStats.CurrentStates.attackSO.power} +({equipStat.damage})";
+        attackSpeedStatText.text = AttackSpeedResult(playerStats.CurrentStates.attackSO.delay,equipStat.atkSpeed) + "/s";
+        maxHpStatText.text = $"{playerStats.CurrentStates.maxHealth} +({equipStat.hp}))";
+        maxStanminaStatText.text = $"{playerStats.CurrentStates.maxStamina} +({equipStat.stamina})";
+        defStatText.text = $"{playerStats.CurrentStates.maxDefense} +({equipStat.def})";
+        moveSpeedText.text = $"{playerStats.CurrentStates.speed} +({equipStat.speed})";
+
+        deathCount.text = Managers.UserData.playerDeathCount.ToString();
+    }
+
+    private string AttackSpeedResult(float atkSpeed, float equip)
+    {
+        float myAttackSpeed = Mathf.Max(atkSpeed - equip, 0.1f);
+        return myAttackSpeed.ToString();
     }
 }
 
