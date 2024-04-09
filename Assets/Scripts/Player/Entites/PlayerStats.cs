bbd0f2f7710c7ba5e32aa1138ffb1eb342f47d02
jyj5457 <154506328+jyj5457@users.noreply.github.com>
2024-04-09 21:05:34 +0900

    [fix] 스킬과 아이템 스탯 연동

--- Assets/Scripts/Player/Entites/PlayerStats.cs
+++ Assets/Scripts/Player/Entites/PlayerStats.cs
@@ -14,12 +14,16 @@ public enum StatsChangeType
 public class PlayerStats
 {
     public StatsChangeType statsChangeType;
-    [Range(0, 100)] public int maxHealth;
+    [Range(0, 500)] public int maxHealth;
+    [Range(0, 200)] public int itemHealth;
+    public float itemAttack;
     [Range(0f, 20f)] public float speed;
     [Range(0, 100)] public float maxStamina;
     [Range(0, 50)] public float maxDefense;
+    [Range(0, 50)] public float itemDefense;
 
     //공격 데이터
     public AttackSO attackSO;
-
+    
 }
+
