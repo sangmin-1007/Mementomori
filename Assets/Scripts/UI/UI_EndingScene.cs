14fc2f432c3ac70d3a660c590336d0e765b7df86
sangmin-1007 <dosin0822@gmail.com>
2024-04-15 11:54:11 +0900

    refactor : 엔딩씬 텍스트 간격 조정, 총 플레이터임, 죽은 횟수 텍스트 추가

--- Assets/Scripts/UI/UI_EndingScene.cs
+++ Assets/Scripts/UI/UI_EndingScene.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,16 @@ public class UI_EndingScene : UI_Base<UI_EndingScene>
     [SerializeField] private RawImage cinematicMovie;
     [SerializeField] private RectTransform backGround;
 
+    [SerializeField] private Text playTimeText;
+    [SerializeField] private Text deathText;
+
     [SerializeField] private Text endText;
 
-
+    private float time;
 
     private void Start()
     {
+        PlayInfoText();
         StartCoroutine(FadeInOutMovie());
     }
 
@@ -29,7 +34,7 @@ public class UI_EndingScene : UI_Base<UI_EndingScene>
 
         cinematicMovie.gameObject.SetActive(false);
 
-        backGround.DOAnchorPosY(930f, 30f);
+        backGround.DOAnchorPosY(1400f, 30f);
         yield return new WaitForSeconds(30f);
 
 
@@ -42,5 +47,15 @@ public class UI_EndingScene : UI_Base<UI_EndingScene>
                 Application.Quit();
 #endif
         yield break;
+    }
+
+    private void PlayInfoText()
+    {
+        time = Managers.GameManager.totalPlayTime;
+
+        var seconds = TimeSpan.FromSeconds(time);
+
+        playTimeText.text = $"당신의 플레이 타임은\n {seconds.Days}일 {seconds.Hours}시간, {seconds.Minutes}분 이며";
+        deathText.text = $"{Managers.UserData.playerDeathCount}번 죽으셨습니다.";
     }
 }
