c11bd161dc7a95c2d8319595abc11baed8fbb069
sangmin-1007 <dosin0822@gmail.com>
2024-04-16 12:28:10 +0900

    remove : 사용하지 않는 스크립트 제거

--- Assets/Scripts/Managers/SkillDataManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SkillDataManager : MonoBehaviour
-{
-     public List<SkillData> playerSkillData = new List<SkillData>();
-}
