d842304c0c35e6b01ba01b701f9c5e8ae424c51b
sangmin-1007 <dosin0822@gmail.com>
2024-04-23 10:22:49 +0900

    feat : 새로운 스킬 추가
    무기를 y축으로 던지는 스킬 추가

---
+++ Assets/Scripts/Player/Skill/Skill_ThrowWeapon.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_ThrowWeapon : MonoBehaviour
+{
+    [SerializeField] private GameObject sowerd;
+
+    [SerializeField] private Sprite[] sowerdSprite;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float power;
+    [SerializeField] private float rotate;
+
+    private GameObject player;
+    private float time = 0f;
+    private void Awake()
+    {
+        player = Managers.GameSceneManager.Player;
+    }
+    private void OnEnable()
+    {
+        transform.position = player.transform.position;
+        int randIndex = Random.Range(0,sowerdSprite.Length);
+        spriteRenderer.sprite = sowerdSprite[randIndex];
+
+        Vector2 weaponDir = Vector2.up;
+        int rand = Random.Range(0, 3);
+
+        weaponDir.x -= 0.2f;
+        weaponDir.x += 0.2f * rand;
+
+        rb.AddForce(weaponDir * power);
+    }
+
+    private void Update()
+    {
+        if (!sowerd.activeSelf)
+            return;
+
+        transform.Rotate(Vector3.forward * rotate * Time.deltaTime); 
+        DisableSkill();
+    }
+
+    private void DisableSkill()
+    {
+        time += Time.deltaTime;
+
+        if (time >= 3)
+        {
+            sowerd.SetActive(false);
+            time = 0;
+        }
+    }
+
+}
